8ec6eed6595ef78a68396cc97b47332871d535e5
Thirawat (First) Prasert <tpr@manaosoftware.com>
2021-01-28 13:08:53 +0700

    Add refresh function

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -15,6 +15,10 @@ class UserProductsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
 
+    Future<void> _refreshProducts(BuildContext context) async {
+      await Provider.of<Products>(context, listen: false).fetchAndSetProducts();
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Products'),
@@ -27,19 +31,22 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListView.builder(
-          itemCount: productsData.items.length,
-          itemBuilder: (_, i) => Column(
-            children: [
-              UserProductItem(
-                productsData.items[i].id,
-                productsData.items[i].title,
-                productsData.items[i].imageUrl,
-              ),
-              Divider(),
-            ],
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListView.builder(
+            itemCount: productsData.items.length,
+            itemBuilder: (_, i) => Column(
+              children: [
+                UserProductItem(
+                  productsData.items[i].id,
+                  productsData.items[i].title,
+                  productsData.items[i].imageUrl,
+                ),
+                Divider(),
+              ],
+            ),
           ),
         ),
       ),
